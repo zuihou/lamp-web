84a9cdfabfbcd57888c6a905a0e995004577dca7
tangyh <244387066@qq.com>
2019-12-13 15:36:45 +0800

    测试提交

--- src/views/zuihou/auth/menu/Edit.vue
+++ src/views/zuihou/auth/menu/Edit.vue
@@ -7,68 +7,35 @@
     :width="width"
     top="50px"
   >
-    <el-form
-      ref="form"
-      :model="resource"
-      :rules="rules"
-      label-position="right"
-      label-width="100px"
-    >
-      <el-form-item
-        :label="$t('table.resource.code')"
-        prop="code"
-      >
+    <el-form :model="resource" :rules="rules" label-position="right" label-width="100px" ref="form">
+      <el-form-item :label="$t('table.resource.code')" prop="code">
         <el-input
-          v-model="resource.code"
           :disabled="type==='edit'"
           @keyup.enter.native="submitForm"
+          v-model="resource.code"
         />
-        <p class="note">
-          建议使用:作为分隔符，并以view、add、update、delete、export、import、download、upload等关键词结尾
-        </p>
-        <p class="note">
-          如：menu:add、 resource:view、 file:upload
-        </p>
+        <p class="note">建议使用:作为分隔符，并以view、add、update、delete、export、import、download、upload等关键词结尾</p>
+        <p class="note">如：menu:add、 resource:view、 file:upload</p>
       </el-form-item>
-      <el-form-item
-        :label="$t('table.resource.name')"
-        prop="name"
-        @keyup.enter.native="submitForm"
-      >
+      <el-form-item :label="$t('table.resource.name')" @keyup.enter.native="submitForm" prop="name">
         <el-input v-model="resource.name" />
       </el-form-item>
       <el-form-item
         :label="$t('table.resource.describe')"
-        prop="describe"
         @keyup.enter.native="submitForm"
+        prop="describe"
       >
         <el-input v-model="resource.describe" />
       </el-form-item>
     </el-form>
-    <div
-      slot="footer"
-      class="dialog-footer"
-    >
-      <el-button
-        plain
-        type="warning"
-        @click="isVisible = false"
-      >
-        {{ $t('common.cancel') }}
-      </el-button>
-      <el-button
-        plain
-        type="primary"
-        @click="submitForm"
-      >
-        {{ $t('common.confirm') }}
-      </el-button>
+    <div class="dialog-footer" slot="footer">
+      <el-button @click="isVisible = false" plain type="warning">{{ $t('common.cancel') }}</el-button>
+      <el-button @click="submitForm" plain type="primary">{{ $t('common.confirm') }}</el-button>
     </div>
   </el-dialog>
 </template>
 <script>
-import resourceApi from '@/api/Resource.js'
-
+import resourceApi from '@/api/Resource.js';
 
 export default {
   name: 'ResourceEdit',
@@ -90,8 +57,17 @@ export default {
       width: this.initWidth(),
       rules: {
         code: [
-          { required: true, message: this.$t('rules.require'), trigger: 'blur' },
-          { min: 1, max: 255, message: this.$t('rules.range4to10'), trigger: 'blur' },
+          {
+            required: true,
+            message: this.$t('rules.require'),
+            trigger: 'blur'
+          },
+          {
+            min: 1,
+            max: 255,
+            message: this.$t('rules.range4to10'),
+            trigger: 'blur'
+          },
           {
             validator: (rule, value, callback) => {
               if (!this.resource.id) {
@@ -106,10 +82,15 @@ export default {
                 // callback()
               }
               callback()
-            }, trigger: 'blur'
+            },
+            trigger: 'blur'
           }
         ],
-        name: { required: true, message: this.$t('rules.require'), trigger: 'blur' }
+        name: {
+          required: true,
+          message: this.$t('rules.require'),
+          trigger: 'blur'
+        }
       }
     }
   },
@@ -124,18 +105,18 @@ export default {
       }
     },
     title () {
-      return this.type === 'add' ? this.$t('common.add') : this.$t('common.edit')
+      return this.type === 'add'
+        ? this.$t('common.add')
+        : this.$t('common.edit')
     }
   },
-  watch: {
-
-  },
+  watch: {},
   mounted () {
     window.onresize = () => {
       return (() => {
         this.width = this.initWidth()
       })()
-    }
+    };
   },
   methods: {
     initResource () {
@@ -149,17 +130,17 @@ export default {
     initWidth () {
       this.screenWidth = document.body.clientWidth
       if (this.screenWidth < 991) {
-        return '90%'
+        return '90%';
       } else if (this.screenWidth < 1400) {
-        return '45%'
+        return '45%';
       } else {
-        return '800px'
+        return '800px';
       }
     },
     setResource (val) {
-      const vm = this
+      const that = this
       if (val) {
-        vm.resource = { ...val }
+        that.resource = { ...val }
       }
     },
     close () {
@@ -172,40 +153,39 @@ export default {
       this.resource = this.initResource()
     },
     submitForm () {
-      const vm = this
-      this.$refs.form.validate((valid) => {
+      const that = this
+      this.$refs.form.validate(valid => {
         if (valid) {
-          vm.editSubmit()
+          that.editSubmit()
         } else {
           return false
         }
       })
     },
     editSubmit () {
-      const vm = this
-      if (vm.type === 'add') {
-        vm.save()
+      const that = this
+      if (that.type === 'add') {
+        that.save()
       } else {
-        vm.update()
+        that.update()
       }
     },
     save () {
-      const vm = this
-      resourceApi.save(this.resource)
-        .then((response) => {
-          const res = response.data
-          if (res.isSuccess) {
-            vm.isVisible = false
-            vm.$message({
-              message: vm.$t('tips.createSuccess'),
-              type: 'success'
-            })
-            vm.$emit('success')
-          }
-        })
+      const that = this
+      resourceApi.save(this.resource).then(response => {
+        const res = response.data
+        if (res.isSuccess) {
+          that.isVisible = false
+          that.$message({
+            message: that.$t('tips.createSuccess'),
+            type: 'success'
+          })
+          that.$emit('success')
+        }
+      })
     },
     update () {
-      resourceApi.update(this.resource).then((response) => {
+      resourceApi.update(this.resource).then(response => {
         const res = response.data
         if (res.isSuccess) {
           this.isVisible = false
