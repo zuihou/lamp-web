b188a9f5dd2d4f24235c21cd41405aa3f4a332c8
tangyh <244387066@qq.com>
2019-12-13 15:37:29 +0800

    去除提交代码时检测功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,9 @@ module.exports = {
     'plugin:vue/essential', 'plugin:vue/recommended', 'eslint:recommended'
   ],
   rules: {
+    // 还可以写表达式，厉害了~
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
