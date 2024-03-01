80585ab9075833ad80468b9a7d623b184fa938ca
zuihou <244387066@qq.com>
2024-03-01 15:46:57 +0800

    feat: 优化代码

--- src/views/lamp/home/components/WorkbenchHeader.vue
+++ src/views/lamp/home/components/WorkbenchHeader.vue
@@ -2,13 +2,13 @@
   <div class="lg:flex">
     <AvatarPreview
       :fileId="userinfo.avatarId"
-      :errorTxt="userinfo?.nickName?.substr(0, 2)"
+      :errorTxt="userinfo?.nickName?.substring(0, 2)"
       :size="72"
       :isDef="true"
       class="!mx-auto !block"
     />
     <div class="md:ml-6 flex flex-col justify-center md:mt-0 mt-2">
-      <h1 class="md:text-lg text-md">早安, {{ userinfo.nickName }}, 开始您一天的工作吧！</h1>
+      <h1 class="md:text-lg text-md">早安, {{ userinfo?.nickName }}, 开始您一天的工作吧！</h1>
       <span class="text-secondary"> {{ userinfo?.workDescribe ?? '这家伙很懒什么也没留下' }} </span>
     </div>
     <div class="flex flex-1 justify-end md:mt-0 mt-4">
