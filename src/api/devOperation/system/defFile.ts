341d480e788fe573d2cf87316fb50a12e341db1e
zuihou <244387066@qq.com>
2025-02-18 21:40:13 +0800

    fix: 下载文件接口对应后台接口

--- src/api/devOperation/system/defFile.ts
+++ src/api/devOperation/system/defFile.ts
@@ -19,7 +19,7 @@ export const Api = {
   } as AxiosRequestConfig,
 
   Download: {
-    url: `${ServicePrefixEnum.FILE}/anyTenant/file/download`,
+    url: `${ServicePrefixEnum.FILE}/anyone/file/download`,
     method: RequestEnum.GET,
     responseType: 'blob',
   } as AxiosRequestConfig,
